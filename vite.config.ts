d01a0454afd7c6b3ad7c9bbae9f3e3557e5dca22
andersonfarias-collab <anderson.farias@gmail.com>
2025-09-07 17:52:31 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,22 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig({
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  preview: {
+    host: "::",
+    port: 8080,
+    allowedHosts: ["web-4vesg.knkugw.easypanel.host","4visionesg.com.br"],
+  },
+  plugins: [react()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  plugins: [react()],
-  preview: {
-    host: true, // Necessário para ser acessível no container
-    allowedHosts: [
-      'web-dominos.knkugw.easypanel.host',
-      'domino.sjumper.qzz.io',
-    ],
-    },
-});
+}));
+
