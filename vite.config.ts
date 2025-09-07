83d38142dfca3731749d77ebc54cee5da6852a4e
andersonfarias-collab <anderson.farias@gmail.com>
2025-09-07 17:24:28 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,11 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-plugins: [react()],
+  // ...suas configs
   preview: {
-    host: true, // Necessário para ser acessível no container
-    // A linha mais importante:
-    allowedHosts: ['*','web-4vesg.knkugw.easypanel.host','4visionesg.com.br'],
+    allowedHosts: [
+      'web-4vesg.knkugw.easypanel.host'
+    ],
+    port: 80 // se estiver usando essa porta
   }
-});
+})
